0d8ec3795ad5089f667267b646953310a5aaa5b4
Shtha-Mohd <shtah.radwan.op@gmail.com>
2023-06-16 11:23:20 -0700

    Betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /** 
- * main - see if random number is positive, negative or zero.
+ * main- see if random number is positive, negative or zero.
  *
  * Return: Always 0(Success)
  */
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{ 
-		printf("%d is positive\n" , n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	       	printf("%d is zero\n" , n);
+	       	printf("%d is zero\n", n);
 	}
 	else
 	{
-	       	printf("%d is negative\n" , n);
+	       	printf("%d is negative\n", n);
 	}
 	
 		
