e79661ac0e24448e3986b6bf3987c8133bbe5c31
Shtha-Mohd <shtah.radwan.op@gmail.com>
2023-06-16 17:53:26 -0700

    print_ comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - comb3.
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i=48;
+	int l=48;
+
+	for (i < 57; l < 58; l++)
+	{
+		if (i < l)
+		{
+			putchar(i);
+			putchar(l);
+			if (i != 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		if (l==57 && i<57)
+		{
+			i++;
+			l =48;
+		}
+	}
+	return (0);
+}
